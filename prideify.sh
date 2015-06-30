0e9c88c07b36daa4bcd1dcc0687c77ef64faea09
Jonathan Beall <JiBBerish@gmail.com>
2015-06-30 12:44:58 -0600

    More quoting of filenames: spaces have pride too.

--- prideify.sh
+++ prideify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # prideify.sh
 
+set -e # Exit on failures.
+
 usage () {
   echo 'Usage: ./prideify.sh [-r|-b|-p|-t] <input image file>. Output will be at <input image file>-pridified. The optional -r, -b, -p, or -t flags switch between rainbow, bisexual, pansexual, and transgender pride flags, with -r as the default.'
   exit -1
@@ -26,16 +28,16 @@ else
     usage
 fi
 
-if [ -z $image ]
+if [ -z "$image" ]
 then
     usage
 fi
 
-width=$(identify $image | sed 's/.* \([0-9][0-9]*\)x\([0-9][0-9]*\) .*/\1/')
-height=$(identify $image | sed 's/.* \([0-9][0-9]*\)x\([0-9][0-9]*\) .*/\2/')
+width=$(identify "$image" | sed 's/.* \([0-9][0-9]*\)x\([0-9][0-9]*\) .*/\1/')
+height=$(identify "$image" | sed 's/.* \([0-9][0-9]*\)x\([0-9][0-9]*\) .*/\2/')
 tmp=$(mktemp 2>/dev/null || mktemp -t 'tmp')
 
-cp $image $tmp
+cp "$image" "$tmp"
 
 
 # rect height = ceil(height/# of rows) - 1, ceil to avoid an empty line across the bottom for non-divisible image heights and - 1 because each row starts a pixel below the previous one.
@@ -46,13 +48,13 @@ y1=0
 for color in ${colors[@]}
 do
   let "y2=$y1+$recth"
-  convert $tmp -strokewidth 0 -fill "rgba($color,0.5)" -draw "rectangle 0,$y1 $width,$y2" $tmp
+  convert "$tmp" -strokewidth 0 -fill "rgba($color,0.5)" -draw "rectangle 0,$y1 $width,$y2" "$tmp"
   let "y1=y2+1" # Stripes are non-overlapping.
 done
 
 if [[ `basename "$image"` =~ '.' ]]
 then
-    mv $tmp "${image%.*}"-prideified."${image##*.}"
+    mv "$tmp" "${image%.*}"-prideified."${image##*.}"
 else
-    mv $tmp "$image-prideified"
+    mv "$tmp" "$image-prideified"
 fi
