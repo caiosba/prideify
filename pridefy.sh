189760dd0a9fdaa6a96d5314e3f99147c8cb0211
Jonathan Beall <JiBBerish@gmail.com>
2015-06-30 04:08:48 -0600

    Stripes were overlapping by 1 px, so bright lines.
    Fix off-by-1 errors in row border calculations: the start of each row is now 1 pixel beyond the previous, stripe height is reduced by 1.  Since rows go from 0 to height - 1, it's okay that we never draw a stripe to row # height when height % 6 == 0, we reach row # height - 1 in all cases.

--- pridefy.sh
+++ pridefy.sh
@@ -16,8 +16,9 @@ colors='255,62,24 252,154,0 255,216,0 57,234,124 11,178,255 152,90,255'
 
 cp $image $tmp
 
-# rect height = ceil(height/# of rows), to avoid an empty line across the bottom for non-divisible image heights.
-let "recth=($height - 1)/6 + 1"
+
+# rect height = ceil(height/# of rows) - 1, ceil to avoid an empty line across the bottom for non-divisible image heights and - 1 because each row starts a pixel below the previous one.
+let "recth=($height - 1)/6"
 
 y1=0
 
@@ -25,7 +26,7 @@ for color in $colors
 do
   let "y2=$y1+$recth"
   convert $tmp -strokewidth 0 -fill "rgba($color,0.5)" -draw "rectangle 0,$y1 $width,$y2" $tmp
-  let "y1=y2"
+  let "y1=y2+1" # Stripes are non-overlapping.
 done
 
 mv $tmp "$image-prideified"
